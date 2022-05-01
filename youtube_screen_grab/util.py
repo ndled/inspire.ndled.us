dd669138ac1b44d6ab5d47be146d148b7efb57ee
ndled <noah@ndled.us>
2022-05-01 16:41:13 -0500

    App starting place

---
+++ youtube_screen_grab/util.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import logging
+import os
+import random
+import math
+
+from yt_dlp import YoutubeDL
+import cv2
+
+logging.basicConfig(
+    level=logging.INFO, format="[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+
+def remove_old(dir):
+    for file in os.listdir(dir):
+        os.remove(f"{dir}/{file}")
+
+
+def random_image(img_dir = "./static/imgs"):
+    """
+    Return a random image from the ones in the static/ directory
+    """
+    img_list = [f for f in os.listdir(img_dir) if f.endswith(".jpg")]
+    img_path = random.choice(img_list)
+    return img_path
+
+
+def download(url, path = "static/single"):
+    ydl_opts = {
+        'outtmpl': f"{path}/{'%(title)s-%(id)s.%(ext)s'}",
+        'overwrites': True,
+        'format':'135' # Fun fact, has to be string
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+
+
+def video_cut(src, destination):
+    for file in os.listdir(src):
+        video_path = f"{src}/{file}"
+        image_name = os.path.basename(video_path)
+        cap = cv2.VideoCapture(video_path)
+        frame_rate = cap.get(5)
+        count = 0
+        while(cap.isOpened()):
+            frame_id = cap.get(1)
+            ret, frame = cap.read()
+            if (ret != True):
+                break
+            if (frame_id % math.floor(frame_rate)*60 == 0):
+                name =image_name + str(count).zfill(5) + ".jpg";count+=1            
+                cv2.imwrite(f"{destination}/{name}", frame)
+    cap.release()
---
+++ youtube_screen_grab/views.py
@@ -0,0 +1,31 @@
+from flask import render_template
+from flask import request
+from flask import Blueprint
+import os
+
+from youtube_screen_grab.util import download, remove_old, video_cut, random_image
+
+bp = Blueprint('/new', __name__)
+
+@bp.route("/new", methods=['GET', 'POST'])
+def new():
+    if request.method == 'POST':
+        if request.form.get("submit"):
+            form_data = request.form
+            url = form_data['new_video']
+            if os.path.exists("./youtube_screen_grab/static/temp/"):
+                remove_old("./youtube_screen_grab/static/temp/")
+            else:
+                os.makedirs("./youtube_screen_grab/static/temp/")
+            download(url, "./youtube_screen_grab/static/temp")
+            video_path = "./youtube_screen_grab/static/temp/"
+            video_cut(video_path,"./youtube_screen_grab/static/temp" )
+            image_path = random_image("./youtube_screen_grab/static/temp")
+            return render_template('new.html', image = f"static/temp/{image_path}")
+        elif request.form["random_image"]:
+            image_path = random_image(img_dir = "./youtube_screen_grab/static/temp")
+            return render_template('new.html', image = f"static/temp/{image_path}")
+    else:
+        return render_template('new.html')
+
+
