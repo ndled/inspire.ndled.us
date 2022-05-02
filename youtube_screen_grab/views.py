95a5574bf2c2c8be1ff6d17387a14e3daafa20e5
ndled <noah@ndled.us>
2022-05-02 18:10:35 -0500

    black

--- youtube_screen_grab/views.py
+++ youtube_screen_grab/views.py
@@ -33,28 +33,41 @@ def new():
             url_id = url.split("v=")[-1]
             if url_id in os.listdir("./youtube_screen_grab/static/temp/"):
                 return redirect(url_for("/.newurl", url_id=url_id))
-            else: 
+            else:
                 task = new_video.delay(url)
                 return redirect(url_for("/.taskstatus", task_id=task.id))
     else:
-        results = [f for f in os.listdir("./youtube_screen_grab/static/temp/") if os.path.isdir(f"./youtube_screen_grab/static/temp/{f}")]
+        results = [
+            f
+            for f in os.listdir("./youtube_screen_grab/static/temp/")
+            if os.path.isdir(f"./youtube_screen_grab/static/temp/{f}")
+        ]
         print(results)
-        return render_template("new.html", results = results)
+        return render_template("new.html", results=results)
 
 
 @bp.route("/taskstatus/<task_id>", methods=["GET", "POST"])
 def taskstatus(task_id):
     task = new_video.AsyncResult(task_id)
     if task.state != "SUCCESS":
-        return render_template("task.html", video_name = "In Progress, this can take a while. Press random image to see if it's done", status = task.state)
+        return render_template(
+            "task.html",
+            video_name="In Progress, this can take a while. Press random image to see if it's done",
+            status=task.state,
+        )
     else:
-        print(task.result.split('v=')[-1])
-        return redirect(url_for("/.newurl", url_id=task.result.split('v=')[-1]))
+        print(task.result.split("v=")[-1])
+        return redirect(url_for("/.newurl", url_id=task.result.split("v=")[-1]))
+
 
 @bp.route("/<url_id>", methods=["GET", "POST"])
 def newurl(url_id):
     if os.path.exists(f"./youtube_screen_grab/static/temp/{url_id}"):
         image_path = random_image(img_dir=f"./youtube_screen_grab/static/temp/{url_id}")
-        return render_template("task.html", image=f"./static/temp/{url_id}/{image_path}", video_name = image_path.split('.mp4')[0])
+        return render_template(
+            "task.html",
+            image=f"./static/temp/{url_id}/{image_path}",
+            video_name=image_path.split(".mp4")[0],
+        )
     else:
         return redirect(url_for("/.new"))
