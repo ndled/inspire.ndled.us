4f05ab749e1b571f2123226f6a35bfafe1408555
ndled <noah@ndled.us>
2022-05-03 14:01:15 -0500

    black

--- youtube_screen_grab/views.py
+++ youtube_screen_grab/views.py
@@ -39,7 +39,11 @@ def new():
             if len(folder) == 11:
                 if os.listdir(f"./youtube_screen_grab/static/temp/{folder}"):
                     youtube_id.append(folder)
-                    youtube_names.append(os.listdir(f"./youtube_screen_grab/static/temp/{folder}")[0].split(folder)[0])
+                    youtube_names.append(
+                        os.listdir(f"./youtube_screen_grab/static/temp/{folder}")[
+                            0
+                        ].split(folder)[0]
+                    )
     if request.method == "POST":
         if request.form.get("submit"):
             form_data = request.form
@@ -48,14 +52,14 @@ def new():
                 return redirect(url_for("/.newurl", url_id=url_id))
             elif len(url_id) == 11:
                 task = new_video.delay(url, url_id)
-                return redirect(url_for("/.taskstatus", task_id=task.id, url_id = url_id))
+                return redirect(url_for("/.taskstatus", task_id=task.id, url_id=url_id))
             flash("Nope! Should be a valid youtube url")
-            return render_template("new.html", results=youtube_id, name = youtube_names)
+            return render_template("new.html", results=youtube_id, name=youtube_names)
         else:
             flash("need a url")
-            return render_template("new.html", results=youtube_id, name = youtube_names)
+            return render_template("new.html", results=youtube_id, name=youtube_names)
     else:
-        return render_template("new.html", results=youtube_id, name = youtube_names)
+        return render_template("new.html", results=youtube_id, name=youtube_names)
 
 
 @bp.route("/taskstatus/<task_id>_<url_id>", methods=["GET", "POST"])
@@ -70,7 +74,7 @@ def taskstatus(task_id, url_id):
             "task.html",
             video_name="In Progress, this can take a while. Press random image to see if it's done",
             status=task.state,
-            result_url = url_id
+            result_url=url_id,
         )
     else:
         return redirect(url_for("/.newurl", url_id=task.result.split("v=")[-1]))
@@ -80,7 +84,9 @@ def taskstatus(task_id, url_id):
 def newurl(url_id):
     if os.path.exists(f"./youtube_screen_grab/static/temp/{url_id}"):
         if len(os.listdir(f"./youtube_screen_grab/static/temp/{url_id}")) > 1:
-            image_path = random_image(img_dir=f"./youtube_screen_grab/static/temp/{url_id}")
+            image_path = random_image(
+                img_dir=f"./youtube_screen_grab/static/temp/{url_id}"
+            )
             return render_template(
                 "task.html",
                 image=f"./static/temp/{url_id}/{image_path}",
@@ -88,8 +94,6 @@ def newurl(url_id):
             )
         else:
             flash("working on it")
-            return render_template(
-                "task.html"
-            )          
+            return render_template("task.html")
     else:
         return redirect(url_for("/.new"))
